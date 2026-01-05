ebb182ec2c7e2a79decea3754481455bbcfcd6ba
krakenhavoc <krakenhavoc@protonmail.com>
2026-01-05 11:41:24 -0500

    completed lecture6

---
+++ coursework/Class-basedProgramDesign/Lectures/6/src/AncestryTree.java
@@ -0,0 +1,96 @@
+import tester.Tester;
+
+interface IAT {
+	int count();
+	int countHelper();
+	int countFemaleAnc();
+	int countFemaleAncHelper();
+}
+
+class Unknown implements IAT {
+	Unknown() {
+	}
+
+	public int count() {
+		return 0;
+	}
+
+	public int countHelper() {
+		return 0;
+	}
+
+	public int countFemaleAnc() {
+		return 0;
+	}
+	
+	public int countFemaleAncHelper() {
+		return 0;
+	}
+}
+
+class Person implements IAT {
+	String name;
+	int yob;
+	boolean isMale;
+	IAT mom;
+	IAT dad;
+
+	Person(String name, int yob, boolean isMale, IAT mom, IAT dad) {
+		this.name = name;
+		this.yob = yob;
+		this.isMale = isMale;
+		this.mom = mom;
+		this.dad = dad;
+	}
+
+	public int count() {
+		return this.mom.countHelper() + this.dad.countHelper();
+	}
+
+	public int countHelper() {
+		return 1 + this.mom.countHelper() + this.dad.countHelper();
+	}
+	
+	public int countFemaleAnc() {
+		return this.mom.countFemaleAncHelper() + this.dad.countFemaleAncHelper();
+	}
+	public int countFemaleAncHelper() {
+		if ( this.isMale == false ) {
+			return 1 + this.mom.countFemaleAncHelper() + this.dad.countFemaleAncHelper();
+		} else {
+			return this.mom.countFemaleAncHelper() + this.dad.countFemaleAncHelper();
+		}
+	}
+}
+
+class ExamplesIAT {
+	IAT enid = new Person("Enid", 1904, false, new Unknown(), new Unknown());
+	IAT edward = new Person("Edward", 1902, true, new Unknown(), new Unknown());
+	IAT emma = new Person("Emma", 1906, false, new Unknown(), new Unknown());
+	IAT eustace = new Person("Eustace", 1907, true, new Unknown(), new Unknown());
+
+	IAT david = new Person("David", 1925, true, new Unknown(), this.edward);
+	IAT daisy = new Person("Daisy", 1927, false, new Unknown(), new Unknown());
+	IAT dana = new Person("Dana", 1933, false, new Unknown(), new Unknown());
+	IAT darcy = new Person("Darcy", 1930, false, this.emma, this.eustace);
+	IAT darren = new Person("Darren", 1935, true, this.enid, new Unknown());
+	IAT dixon = new Person("Dixon", 1936, true, new Unknown(), new Unknown());
+
+	IAT clyde = new Person("Clyde", 1955, true, this.daisy, this.david);
+	IAT candace = new Person("Candace", 1960, false, this.dana, this.darren);
+	IAT cameron = new Person("Cameron", 1959, true, new Unknown(), this.dixon);
+	IAT claire = new Person("Claire", 1956, false, this.darcy, new Unknown());
+
+	IAT bill = new Person("Bill", 1980, true, this.candace, this.clyde);
+	IAT bree = new Person("Bree", 1981, false, this.claire, this.cameron);
+
+	IAT andrew = new Person("Andrew", 2001, true, this.bree, this.bill);
+
+	boolean testIATCount(Tester t) {
+		return t.checkExpect(andrew.count(), 16);
+	}
+	
+	boolean testIATFemaleAnc(Tester t) {
+		return t.checkExpect(andrew.countFemaleAnc(), 8);
+	}
+}
