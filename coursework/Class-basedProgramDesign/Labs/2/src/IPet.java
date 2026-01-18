888e35b8c287c1188f10da6a004c6848d6d9b915
krakenhavoc <krakenhavoc@protonmail.com>
2026-01-18 09:14:12 -0500

    reformat through eclipse

--- coursework/Class-basedProgramDesign/Labs/2/src/IPet.java
+++ coursework/Class-basedProgramDesign/Labs/2/src/IPet.java
@@ -2,95 +2,94 @@ import tester.Tester;
 
 //to represent a pet owner
 class Owner {
-	String name;
-	IPet pet;
-	int age;
+  String name;
+  IPet pet;
+  int age;
 
-	Owner(String name, IPet pet, int age) {
-		this.name = name;
-		this.pet = pet;
-		this.age = age;
-	}
+  Owner(String name, IPet pet, int age) {
+    this.name = name;
+    this.pet = pet;
+    this.age = age;
+  }
 
-	// is this Owner older than the given Owner?
-	boolean isOlder(Owner other) {
-		return this.age > other.age;
-	}
+  // is this Owner older than the given Owner?
+  boolean isOlder(Owner other) {
+    return this.age > other.age;
+  }
 
-	// does the name of this Owner's pet match the given name?
-	boolean sameNamePet(Owner other) {
-		return this.pet.getName() == other.pet.getName();
-	}
-	
-	Owner perish() {
-		return new Owner(this.name, new NoPet(), this.age);
-	}
+  // does the name of this Owner's pet match the given name?
+  boolean sameNamePet(Owner other) {
+    return this.pet.getName() == other.pet.getName();
+  }
+
+  Owner perish() {
+    return new Owner(this.name, new NoPet(), this.age);
+  }
 }
 
-//to represent a pet
+// to represent a pet
 interface IPet {
-	String getName();
+  String getName();
 }
 
-//to represent a pet cat
+// to represent a pet cat
 class Cat implements IPet {
-	String name;
-	String kind;
-	boolean longhaired;
+  String name;
+  String kind;
+  boolean longhaired;
 
-	Cat(String name, String kind, boolean longhaired) {
-		this.name = name;
-		this.kind = kind;
-		this.longhaired = longhaired;
-	}
+  Cat(String name, String kind, boolean longhaired) {
+    this.name = name;
+    this.kind = kind;
+    this.longhaired = longhaired;
+  }
 
-	public String getName() {
-		return this.name;
-	}
+  public String getName() {
+    return this.name;
+  }
 }
 
-//to represent a pet dog
+// to represent a pet dog
 class Dog implements IPet {
-	String name;
-	String kind;
-	boolean male;
+  String name;
+  String kind;
+  boolean male;
 
-	Dog(String name, String kind, boolean male) {
-		this.name = name;
-		this.kind = kind;
-		this.male = male;
-	}
+  Dog(String name, String kind, boolean male) {
+    this.name = name;
+    this.kind = kind;
+    this.male = male;
+  }
 
-	public String getName() {
-		return this.name;
-	}
+  public String getName() {
+    return this.name;
+  }
 }
 
 class NoPet implements IPet {
-	NoPet() {
-	}
+  NoPet() {
+  }
 
-	public String getName() {
-		return "";
-	}
+  public String getName() {
+    return "";
+  }
 }
 
 class ExamplesOwner {
-	IPet nala = new Cat("Nala", "Domestic Shorthair", false);
-	IPet pazuzu = new Cat("Pazuzu", "Mainecoon", true);
-	IPet luna = new Dog("Luna", "Cockapoo", false);
-	IPet flynn = new Dog("Flynn", "Mixed Goodboy", true);
-	IPet none = new NoPet();
+  IPet nala = new Cat("Nala", "Domestic Shorthair", false);
+  IPet pazuzu = new Cat("Pazuzu", "Mainecoon", true);
+  IPet luna = new Dog("Luna", "Cockapoo", false);
+  IPet flynn = new Dog("Flynn", "Mixed Goodboy", true);
+  IPet none = new NoPet();
 
-	Owner luke = new Owner("Luke", nala, 30);
-	Owner seema = new Owner("Seema", pazuzu, 40);
-	Owner matt = new Owner("Matt", luna, 28);
-	Owner rach = new Owner("Rachel", flynn, 25);
-	Owner bev = new Owner("Beverly", none, 60);
+  Owner luke = new Owner("Luke", nala, 30);
+  Owner seema = new Owner("Seema", pazuzu, 40);
+  Owner matt = new Owner("Matt", luna, 28);
+  Owner rach = new Owner("Rachel", flynn, 25);
+  Owner bev = new Owner("Beverly", none, 60);
 
-	boolean testMotMeetsFuelEfficiency(Tester t) {
-		return t.checkExpect(luke.sameNamePet(luke), true)
-				&& t.checkExpect(luke.sameNamePet(seema), false)
-				&& t.checkExpect(matt.sameNamePet(bev), false);
-	}
+  boolean testMotMeetsFuelEfficiency(Tester t) {
+    return t.checkExpect(luke.sameNamePet(luke), true) && t.checkExpect(luke.sameNamePet(seema), false)
+        && t.checkExpect(matt.sameNamePet(bev), false);
+  }
 }
--- coursework/Class-basedProgramDesign/Lectures/6/src/AncestryTree.java
+++ coursework/Class-basedProgramDesign/Lectures/6/src/AncestryTree.java
@@ -1,126 +1,126 @@
 import tester.Tester;
 
 interface IAT {
-	int count();
+  int count();
 
-	int countHelper();
+  int countHelper();
 
-	int countFemaleAnc();
+  int countFemaleAnc();
 
-	int countFemaleAncHelper();
+  int countFemaleAncHelper();
 
-	boolean wellFormed();
+  boolean wellFormed();
 
-	boolean wellFormedHelper(int childYob);
+  boolean wellFormedHelper(int childYob);
 }
 
 class Unknown implements IAT {
-	Unknown() {
-	}
+  Unknown() {
+  }
 
-	public int count() {
-		return 0;
-	}
+  public int count() {
+    return 0;
+  }
 
-	public int countHelper() {
-		return 0;
-	}
+  public int countHelper() {
+    return 0;
+  }
 
-	public int countFemaleAnc() {
-		return 0;
-	}
+  public int countFemaleAnc() {
+    return 0;
+  }
 
-	public int countFemaleAncHelper() {
-		return 0;
-	}
+  public int countFemaleAncHelper() {
+    return 0;
+  }
 
-	public boolean wellFormed() {
-		return true;
-	}
+  public boolean wellFormed() {
+    return true;
+  }
 
-	public boolean wellFormedHelper(int childYob) {
-		return true;
-	}
+  public boolean wellFormedHelper(int childYob) {
+    return true;
+  }
 }
 
 class Person implements IAT {
-	String name;
-	int yob;
-	boolean isMale;
-	IAT mom;
-	IAT dad;
+  String name;
+  int yob;
+  boolean isMale;
+  IAT mom;
+  IAT dad;
 
-	Person(String name, int yob, boolean isMale, IAT mom, IAT dad) {
-		this.name = name;
-		this.yob = yob;
-		this.isMale = isMale;
-		this.mom = mom;
-		this.dad = dad;
-	}
+  Person(String name, int yob, boolean isMale, IAT mom, IAT dad) {
+    this.name = name;
+    this.yob = yob;
+    this.isMale = isMale;
+    this.mom = mom;
+    this.dad = dad;
+  }
 
-	public int count() {
-		return this.mom.countHelper() + this.dad.countHelper();
-	}
+  public int count() {
+    return this.mom.countHelper() + this.dad.countHelper();
+  }
 
-	public int countHelper() {
-		return 1 + this.mom.countHelper() + this.dad.countHelper();
-	}
+  public int countHelper() {
+    return 1 + this.mom.countHelper() + this.dad.countHelper();
+  }
 
-	public int countFemaleAnc() {
-		return this.mom.countFemaleAncHelper() + this.dad.countFemaleAncHelper();
-	}
+  public int countFemaleAnc() {
+    return this.mom.countFemaleAncHelper() + this.dad.countFemaleAncHelper();
+  }
 
-	public int countFemaleAncHelper() {
-		if (this.isMale == false) {
-			return 1 + this.mom.countFemaleAncHelper() + this.dad.countFemaleAncHelper();
-		}
-		else {
-			return this.mom.countFemaleAncHelper() + this.dad.countFemaleAncHelper();
-		}
-	}
+  public int countFemaleAncHelper() {
+    if (this.isMale == false) {
+      return 1 + this.mom.countFemaleAncHelper() + this.dad.countFemaleAncHelper();
+    }
+    else {
+      return this.mom.countFemaleAncHelper() + this.dad.countFemaleAncHelper();
+    }
+  }
 
-	public boolean wellFormed() {
-		return this.mom.wellFormedHelper(this.yob) && this.dad.wellFormedHelper(this.yob)
-				&& this.mom.wellFormed() && this.dad.wellFormed();
-	}
+  public boolean wellFormed() {
+    return this.mom.wellFormedHelper(this.yob) && this.dad.wellFormedHelper(this.yob) && this.mom.wellFormed()
+        && this.dad.wellFormed();
+  }
 
-	public boolean wellFormedHelper(int childYob) {
-		return this.yob < childYob;
-	}
+  public boolean wellFormedHelper(int childYob) {
+    return this.yob < childYob;
+  }
 }
 
 class ExamplesIAT {
-	IAT enid = new Person("Enid", 1904, false, new Unknown(), new Unknown());
-	IAT edward = new Person("Edward", 1902, true, new Unknown(), new Unknown());
-	IAT emma = new Person("Emma", 1906, false, new Unknown(), new Unknown());
-	IAT eustace = new Person("Eustace", 1907, true, new Unknown(), new Unknown());
+  IAT enid = new Person("Enid", 1904, false, new Unknown(), new Unknown());
+  IAT edward = new Person("Edward", 1902, true, new Unknown(), new Unknown());
+  IAT emma = new Person("Emma", 1906, false, new Unknown(), new Unknown());
+  IAT eustace = new Person("Eustace", 1907, true, new Unknown(), new Unknown());
 
-	IAT david = new Person("David", 1925, true, new Unknown(), this.edward);
-	IAT daisy = new Person("Daisy", 1927, false, new Unknown(), new Unknown());
-	IAT dana = new Person("Dana", 1933, false, new Unknown(), new Unknown());
-	IAT darcy = new Person("Darcy", 1930, false, this.emma, this.eustace);
-	IAT darren = new Person("Darren", 1935, true, this.enid, new Unknown());
-	IAT dixon = new Person("Dixon", 1936, true, new Unknown(), new Unknown());
+  IAT david = new Person("David", 1925, true, new Unknown(), this.edward);
+  IAT daisy = new Person("Daisy", 1927, false, new Unknown(), new Unknown());
+  IAT dana = new Person("Dana", 1933, false, new Unknown(), new Unknown());
+  IAT darcy = new Person("Darcy", 1930, false, this.emma, this.eustace);
+  IAT darren = new Person("Darren", 1935, true, this.enid, new Unknown());
+  IAT dixon = new Person("Dixon", 1936, true, new Unknown(), new Unknown());
 
-	IAT clyde = new Person("Clyde", 1955, true, this.daisy, this.david);
-	IAT candace = new Person("Candace", 1960, false, this.dana, this.darren);
-	IAT cameron = new Person("Cameron", 1959, true, new Unknown(), this.dixon);
-	IAT claire = new Person("Claire", 1956, false, this.darcy, new Unknown());
+  IAT clyde = new Person("Clyde", 1955, true, this.daisy, this.david);
+  IAT candace = new Person("Candace", 1960, false, this.dana, this.darren);
+  IAT cameron = new Person("Cameron", 1959, true, new Unknown(), this.dixon);
+  IAT claire = new Person("Claire", 1956, false, this.darcy, new Unknown());
 
-	IAT bill = new Person("Bill", 1980, true, this.candace, this.clyde);
-	IAT bree = new Person("Bree", 1981, false, this.claire, this.cameron);
+  IAT bill = new Person("Bill", 1980, true, this.candace, this.clyde);
+  IAT bree = new Person("Bree", 1981, false, this.claire, this.cameron);
 
-	IAT andrew = new Person("Andrew", 2001, true, this.bree, this.bill);
+  IAT andrew = new Person("Andrew", 2001, true, this.bree, this.bill);
 
-	boolean testIATCount(Tester t) {
-		return t.checkExpect(andrew.count(), 16);
-	}
+  boolean testIATCount(Tester t) {
+    return t.checkExpect(andrew.count(), 16);
+  }
 
-	boolean testIATFemaleAnc(Tester t) {
-		return t.checkExpect(andrew.countFemaleAnc(), 8);
-	}
+  boolean testIATFemaleAnc(Tester t) {
+    return t.checkExpect(andrew.countFemaleAnc(), 8);
+  }
 
-	boolean testIATWellFormed(Tester t) {
-		return t.checkExpect(andrew.wellFormed(), true);
-	}
+  boolean testIATWellFormed(Tester t) {
+    return t.checkExpect(andrew.wellFormed(), true);
+  }
 }
